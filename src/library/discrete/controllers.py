d767fd5629ae187092f7af6c48e4231350114323
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-06 15:22:05 +0100

    P controller adapted to new Discrete library.

--- src/library/discrete/controllers.py
+++ src/library/discrete/controllers.py
@@ -7,13 +7,7 @@ class P(library.discrete.Discrete):
 
     def __init__(self, gain, sample_time=-1):
 
-        def f(t, x):
-            return x
-
         def g(x):
             return x * self.u
 
-        self.f = f
-        self.g = g
-
-        super().__init__(x=gain, sample_time=sample_time)
+        super().__init__(x=gain, g=g, sample_time=sample_time)
