4c533e0dcf5b92b4659f864ef2f15c49797dd67c
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-08 13:57:04 +0100

    Three new types of a discrete system.

--- src/library/discrete/__init__.py
+++ src/library/discrete/__init__.py
@@ -3,15 +3,13 @@ import numpy as np
 
 class Discrete:
 
-    def __init__(self, x=None, u=None, f=lambda t, x: x, g=lambda x:
-                 x, sample_time=-1):
+    def __init__(self, x=None, u=None, f=lambda t, x, u: x, g=lambda x, u: x):
 
         self._simulation = None
 
         self.u = u
         self.x = x
         self.last_call = -np.inf
-        self.sample_time = sample_time
 
         # f: the state transition function
         self.f = f
@@ -19,16 +17,51 @@ class Discrete:
         # g: the output function
         self.g = g
 
-    @property
-    def y(self):
-        return self.g(self.x)
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        if name in self.parameters:
+            self._recalculate_aux_vars()
+
+
+class Static(Discrete):
 
     def __call__(self, t, u):
 
-        if t - self.last_call >= self.sample_time:
+        if t - self.last_call >= self.parameters.sample_time:
 
             self.u = u
-            self.x = self.f(t, self.x)
+            self.last_call = t
+
+        return self.y
+
+    @property
+    def y(self):
+        return self.g(self.x, self.u)
+
+
+class NormalOrder(Discrete):
+
+    def __call__(self, t, u):
+
+        if t - self.last_call >= self.parameters.sample_time:
+
+            self.u = u
+            self.y = self.g(self.x, u)
+            self.x = self.f(t, self.x, u)
+            self.last_call = t
+
+        return self.y
+
+
+class ReverseOrder(Discrete):
+
+    def __call__(self, t, u):
+
+        if t - self.last_call >= self.parameters.sample_time:
+
+            self.u = u
+            self.x = self.f(t, self.x, u)
+            self.y = self.g(self.x, u)
             self.last_call = t
 
         return self.y
