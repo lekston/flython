205af79688720f23d9738112ad67ae387b63b1fb
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-18 12:36:49 +0100

    Controllers module after library reconstruction.

--- src/library/controllers.py
+++ src/library/controllers.py
@@ -1,10 +1,7 @@
-import numpy as np
-
-from scipy.integrate import ode
-import block.discrete
+import simulation.blocks.discrete
 
 
-class N(block.discrete.Static):
+class P(simulation.blocks.discrete.Static):
 
     _parameters = ('Kp', 'sample_time', 'dtype')
     _default = dict(sample_time=-1, dtype=[('u', '<f8')])
@@ -13,163 +10,132 @@ class N(block.discrete.Static):
         return self.Kp * u
 
 
+class PID(simulation.blocks.discrete.NormalOrder):
+
+    _parameters = ('Kp', 'Ki', 'Kd', 'sample_time', 'dtype')
+    _default = dict(sample_time=-1, dtype=[('u', '<f8')])
+
+    def f(self, x, u):
+
+        Ci, Cd = self._aux_vars
+
+        # Integral: x[0](t) = yi(t)
+        x[0] = x[0] + Ci * (u + x[2])
+        # Derivative: x[1](t) = yd(t)
+        x[1] = - x[1] + Cd * (u - x[2])
+        # Previous input
+        x[2] = u
+
+        return x
+
+    def g(self, x, u):
+
+        yp = self.Kp * u
+        yi = x[0]
+        yd = x[1]
+
+        return yp + yi + yd
+
+    def _recalculate_aux_vars(self):
+        self._aux_vars = (0.5 * self.Ki * self.sample_time,
+                          2 * self.Kd / self.sample_time)
 
 
-# class P(library.discrete.Static):
+class PIrD(simulation.blocks.discrete.NormalOrder):
 
-#     _parameters = ('Kp', 'sample_time', 'dtype')
-#     _default = dict(sample_time=-1, dtype=[('u', '<f8')])
+    _parameters = ('Kp', 'Ki', 'Kd', 'alpha', 'sample_time', 'dtype')
+    _default = dict(alpha=.1, sample_time=-1, dtype=[('u', '<f8')])
 
-#     def __init__(self, **parameters):
+    def f(self, x, u):
 
-#         def g(x, u):
-#             return self.Kp * u
+        Ci, D1, D2 = self._aux_vars
 
-#         super().__init__(g=g, **parameters)
+        # Integral: x[0](t) = yi(t)
+        x[0] = x[0] + Ci * (u + x[2])
+        # Derivative: x[1](t) = yd(t)
+        x[1] = - D1 * x[1] + D2 * (u - x[2])
+        # Previous input
+        x[2] = u
 
+        return x
 
-# class PID(library.discrete.NormalOrder):
+    def g(self, x, u):
 
-#     _parameters = ('Kp', 'Ki', 'Kd', 'sample_time', 'dtype')
-#     _default = dict(sample_time=-1, dtype=[('u', '<f8')])
+        yp = self.Kp * u
+        yi = x[0]
+        yd = x[1]
 
-#     def __init__(self, **parameters):
+        return yp + yi + yd
 
-#         def f(t, x, u):
-
-#             Ci, Cd = self._aux_vars
-
-#             # Integral: x[0](t) = yi(t)
-#             x[0] = x[0] + Ci * (u + x[2])
-#             # Derivative: x[1](t) = yd(t)
-#             x[1] = - x[1] + Cd * (u - x[2])
-#             # Previous input
-#             x[2] = u
-
-#             return x
-
-#         def g(x, u):
-
-#             yp = self.Kp * u
-#             yi = x[0]
-#             yd = x[1]
-
-#             return yp + yi + yd
-
-#         super().__init__(x=np.zeros(3), f=f, g=g, **parameters)
-
-#     def _recalculate_aux_vars(self):
-#         self._aux_vars = (0.5 * self.Ki * self.sample_time,
-#                           2 * self.Kd / self.sample_time)
+    def _recalculate_aux_vars(self):
+        self._aux_vars = (0.5 * self.Ki * self.sample_time,
+                          (2 * self.alpha - self.sample_time) /
+                          (2 * self.alpha + self.sample_time),
+                          2 * self.Kd / (2 * self.alpha + self.sample_time))
 
 
-# class PIrD(library.discrete.NormalOrder):
+class PIDRealEuler(simulation.blocks.discrete.NormalOrder):
 
-#     _parameters = ('Kp', 'Ki', 'Kd', 'alpha', 'sample_time', 'dtype')
-#     _default = dict(alpha=.1, sample_time=-1, dtype=[('u', '<f8')])
+    _parameters = ('Kp', 'Ki', 'Kd',  'alpha', 'sample_time', 'dtype')
+    _default = dict(sample_time=-1, dtype=[('u', '<f8')])
 
-#     def __init__(self, **parameters):
+    def f(self, x, u):
 
-#         def f(t, x, u):
+        Ci, Cd = self._aux_vars
 
-#             Ci, D1, D2 = self._aux_vars
+        # Integration
+        x[0] = x[0] + Ci * u
 
-#             # Integral: x[0](t) = yi(t)
-#             x[0] = x[0] + Ci * (u + x[2])
-#             # Derivative: x[1](t) = yd(t)
-#             x[1] = - D1 * x[1] + D2 * (u - x[2])
-#             # Previous input
-#             x[2] = u
+        # Differentiation
+        yd = Cd * (u - x[2])
 
-#             return x
+        # Exponential smoothing of the differentiated response
+        x[1] = (1 - self.alpha) * x[1] + self.alpha * yd
 
-#         def g(x, u):
+        # Previous input signal
+        x[2] = u
 
-#             yp = self.Kp * u
-#             yi = x[0]
-#             yd = x[1]
+        return x
 
-#             return yp + yi + yd
+    def g(self, x, u):
 
-#         super().__init__(x=np.zeros(3), f=f, g=g, **parameters)
+        yp = self.Kp * u
+        yi = x[0]
+        yd = x[1]
 
-#     def _recalculate_aux_vars(self):
-#         self._aux_vars = (0.5 * self.Ki * self.sample_time,
-#                           (2 * self.alpha - self.sample_time) /
-#                           (2 * self.alpha + self.sample_time),
-#                           2 * self.Kd / (2 * self.alpha + self.sample_time))
+        return yp + yi + yd
 
-
-# class PIDRealEuler(library.discrete.NormalOrder):
-
-#     _parameters = ('Kp', 'Ki', 'Kd',  'alpha', 'sample_time', 'dtype')
-#     _default = dict(sample_time=-1, dtype=[('u', '<f8')])
-
-#     def __init__(self, **parameters):
-
-#         def f(t, x, u):
-
-#             Ci, Cd = self._aux_vars
-
-#             # Integration
-#             x[0] = x[0] + Ci * u
-
-#             # Differentiation
-#             yd = Cd * (u - x[2])
-
-#             # Exponential smoothing of the differentiated response
-#             x[1] = (1 - self.alpha) * x[1] + self.alpha * yd
-
-#             # Previous input signal
-#             x[2] = u
-
-#             return x
-
-#         def g(x, u):
-
-#             yp = self.Kp * u
-#             yi = x[0]
-#             yd = x[1]
-
-#             return yp + yi + yd
-
-#         super().__init__(x=np.zeros(3), f=f, g=g, **parameters)
-
-#     def _recalculate_aux_vars(self):
-#         self._aux_vars = (self.Ki / self.sample_time,
-#                           self.Kd / self.sample_time)
+    def _recalculate_aux_vars(self):
+        self._aux_vars = (self.Ki / self.sample_time,
+                          self.Kd / self.sample_time)
 
 
-# class PIDss(library.discrete.ReverseOrder):
+class PIDss(simulation.blocks.discrete.ReverseOrder):
 
-#     _parameters = ('Kp', 'Ki', 'Kd', 'sample_time', 'dtype')
-#     _default = dict(sample_time=-1, dtype=[('u', '<f8')])
+    _parameters = ('Kp', 'Ki', 'Kd', 'sample_time', 'dtype')
+    _default = dict(sample_time=-1, dtype=[('u', '<f8')])
 
-#     def __init__(self, **parameters):
+    def f(self, x, u):
 
-#         def f(t, x, u):
+        Ci, Cd = self._aux_vars
 
-#             Ci, Cd = self._aux_vars
+        # Integration
+        x[0] = x[0] + 2 * Ci * u
+        # Derivative
+        x[0] = -x[0] + 2 * Cd * u
 
-#             # Integration
-#             x[0] = x[0] + 2 * Ci * u
-#             # Derivative
-#             x[0] = -x[0] + 2 * Cd * u
+        return x
 
-#             return x
+    def g(self, x, u):
 
-#         def g(x, u):
+        Ci, Cd = self._aux_vars
 
-#             Ci, Cd = self._aux_vars
+        yp = self.Kp * u
+        yi = x[0] + Ci * u
+        yd = x[1] + Cd * u
 
-#             yp = self.Kp * u
-#             yi = x[0] + Ci * u
-#             yd = x[1] + Cd * u
+        return yp + yi + yd
 
-#             return yp + yi + yd
-
-#         super().__init__(x=np.zeros(2), f=f, g=g, **parameters)
-
-#     def _recalculate_aux_vars(self):
-#         self._aux_vars = (0.5 * self.Ki * self.sample_time,
-#                           2 * self.Kd / self.sample_time)
+    def _recalculate_aux_vars(self):
+        self._aux_vars = (0.5 * self.Ki * self.sample_time,
+                          2 * self.Kd / self.sample_time)
