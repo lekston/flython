790ecba21706c8fb1f4696b064f3d08d1f66e93d
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-18 12:46:26 +0100

    Planners module after reconstruction.

--- src/library/aviation/planners.py
+++ src/library/aviation/planners.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from collections import namedtuple
 
-import library.discrete
+import simulation.blocks.discrete
 
 # Windvectors
 WindvectorXZ = namedtuple('Windvector', ['x', 'Vx', 'Vz'])
@@ -14,58 +14,49 @@ WaypointXY = namedtuple('Waypoint', ['x', 'y'])
 WaypointXYZ = namedtuple('Waypoint', ['x', 'y', 'z'])
 
 
-class Constant(library.discrete.Static):
+class Constant(simulation.blocks.discrete.Static):
 
     _parameters = ('setpoint', 'sample_time', 'dtype')
     _default = dict(sample_time=-1, dtype=[('r', '<f8')])
 
-    def __init__(self, **parameters):
-
-        def g(x, u):
-            return self.setpoint
-
-        super().__init__(g=g, **parameters)
+    def g(self, x, u):
+        return self.setpoint
 
 
-class FlightplanXZ(library.discrete.Static):
+class FlightplanXZ(simulation.blocks.discrete.Static):
 
     _parameters = ('plan', 'sample_time', 'dtype')
     _default = dict(sample_time=-1, dtype=[('xr', '<f8'), ('zr', '<f8')])
 
-    def __init__(self, **parameters):
+    def g(self, x, u):
 
-        def g(x, u):
+        # u contains x_vehicle
+        return np.array([u, np.interp(u, self.xinterp, self.zinterp)])
 
-            # u contains x_vehicle
-            return np.array([u, np.interp(u, self.xinterp, self.zinterp)])
-
-        super().__init__(g=g, **parameters)
+    def _expand(self):
 
         self.xinterp = np.array([waypoint.x for waypoint in self.plan])
         self.zinterp = np.array([waypoint.z for waypoint in self.plan])
 
 
-class WindfieldXZ(library.discrete.Static):
+class WindfieldXZ(simulation.blocks.discrete.Static):
 
     _parameters = ('field', 'scale_factor', 'noise_variance',
                    'sample_time', 'dtype')
     _default = dict(scale_factor=1, noise_variance=0, sample_time=-1,
                     dtype=[('Vx', '<f8'), ('Vz', '<f8')])
 
-    def __init__(self, **parameters):
+    def g(self, x, u):
 
-        def g(x, u):
+        # u contains x_vehicle
+        V = self.scale_factor * np.array([
+            np.interp(u, self.xinterp, self.Vxinterp),
+            np.interp(u, self.xinterp, self.Vzinterp)
+        ])
 
-            # u contains x_vehicle
-            V = self.scale_factor * np.array([
-                np.interp(u, self.xinterp, self.Vxinterp),
-                np.interp(u, self.xinterp, self.Vzinterp)
-            ])
+        return V + self.noise_variance * np.random.randn(2)
 
-            return V + self.noise_variance * np.random.randn(2)
-
-        super().__init__(g=g, **parameters)
-
+    def _expand(self):
         self.xinterp = np.array([windvector.x for windvector in self.field])
         self.Vxinterp = np.array([windvector.Vx for windvector in self.field])
         self.Vzinterp = np.array([windvector.Vz for windvector in self.field])
