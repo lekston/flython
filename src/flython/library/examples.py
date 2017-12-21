f71f499e75ef2ff76e1e27a50c7db68a72f7d982
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-21 08:57:50 +0100

    Library planners.py.

---
+++ src/flython/library/examples.py
@@ -0,0 +1,24 @@
+from numpy import zeros
+
+from flython.blockset import Continuous
+
+
+class Motor(Continuous):
+    """Simple second order model of a DC motor (for testing purposes)"""
+
+    _parameters = ('friction', 'dtype')
+    _defaults = dict(dtype=[('phi', '<f8'), ('dphi', '<f8')])
+    _x = zeros(2)
+
+    def f(self, t, x):
+
+        dx = self._x
+        u = self.u
+
+        dx[0] = x[1]
+        dx[1] = u - self.friction*x[1]
+
+        return dx
+
+    def g(self, x):
+        return x[0]
