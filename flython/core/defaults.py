fe9834f7008b2934183c1eb4a81ca79f5a5c05be
Przemyslaw Pietrzak <ppietrzak@flytechuav.com>
2017-12-27 12:36:18 +0100

    Flython core/defaults.py

--- flython/core/defaults.py
+++ flython/core/defaults.py
@@ -1,8 +1,9 @@
+# Local (per simulation) settings
 # Default solver settings
 solver = 'RK45'
 t_beg = 0
 t_end = 10.0
 sample_time = 0.01
 
-# Warnings Filter
-show_warnings = "always"
+# Global settings
+warnings_filter = "interpreter"
